12896bb95190d2ece106d3b3110d709cb0185de1
bryandanielpsp@gmail.com <bryandanielpsp@gmail.com>
2023-05-23 14:19:14 -0400

    fixed members

--- functions/src/mySessions.js
+++ functions/src/mySessions.js
@@ -10,18 +10,21 @@ export async function createAppointment(req, res) {
     game: req.body.game,
     date: req.body.date,
     time: req.body.time,
+    members: req.body.members ? req.body.members : "",
   };
   const newAppoinment = req.body;
   await coll.insertOne(appoinment);
-  res.status(201).send({ message: "new appoinment added" });
+  // res.status(201).send({ message: "new appoinment added" });
+  getAllAppoinments(req, res);
 }
 
-//CRUD : Get
+//CRUD : Get All
 export async function getAllAppoinments(req, res) {
   const myAppoinments = await coll.find({}).toArray();
   res.status(200).send(myAppoinments);
 }
 
+//CRUD : Get
 export async function getAppointment(req, res) {
   const myAppoinment = await coll.findOne({});
   res.status(200).send(myAppoinment);
@@ -32,7 +35,8 @@ export async function deleteAppoinment(req, res) {
   const sessionId = { _id: new ObjectId(req.params.sessionId) };
 
   await coll.deleteOne(sessionId);
-  res.status(200).send({ message: "Session has been deleted." });
+  await getAllAppoinments(req, res);
+  //   res.status(200).send({ message: "Session has been deleted." });
 }
 
 // CRUD: UPDATE
